971efabe403c42aa3fb6c5486e961500600d1e01
Ovie Adese <adeseovie@gmail.com>
2018-10-29 12:13:53 -0400

    variables by references

---
+++ variable-playground.js
@@ -0,0 +1,28 @@
+// var person = {
+//   name: "ovie",
+//   age: 21
+// };
+
+// function updatePerson(obj) {
+//   //   obj = {
+//   //     name: "ovie",
+//   //     age: 23
+//   //   };
+
+//   obj.age = 24;
+// }
+// updatePerson(person);
+// console.log(person);
+
+// Array Example
+
+var grades = [15, 88];
+
+function addGrades(grades) {
+  //   grades.push(55);
+
+  gradesArr = [12, 33, 99];
+}
+
+addGrades(grades);
+console.log(grades);
